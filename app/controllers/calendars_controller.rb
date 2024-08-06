722a740081fbff82f053049b66297930b629065b
haru-teck <ginchatoku1894@gmail.com>
2024-08-06 21:35:43 +0900

    学習用コメント追記

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,6 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
+    # 学習用コメント：スネークケース（全て小文字）
     get_week
     @plan = Plan.new
   end
@@ -34,6 +35,8 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+
+      # 学習用コメント：ハッシュロケットをシンボルに変更
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
