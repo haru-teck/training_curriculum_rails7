2d1238980e3b541457fa602217e18cbb68466369
haru-teck <ginchatoku1894@gmail.com>
2024-08-08 19:55:52 +0900

    ハッシュロケットとタグの修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,10 +37,10 @@ class CalendarsController < ApplicationController
       end
 
       days = {
-         :month => (@todays_date + x).month, 
-         :date => (@todays_date+x).day, 
-         :plans => today_plans,
-         :weekday=> wdays[(@todays_date.wday + x) % 7]
+         month :(@todays_date + x).month, 
+         date : (@todays_date+x).day, 
+         plans : today_plans,
+         weekday: wdays[(@todays_date.wday + x) % 7]
         }
         @week_days.push(days)
 
